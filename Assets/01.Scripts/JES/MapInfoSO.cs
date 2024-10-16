f0db95531a80c2d18a3786a0be9bb13e7f6d038a
jes0321 <jesjes0321@naver.com>
2024-10-16 15:39:02 +0900

    feat : mapinfo sc

---
+++ Assets/01.Scripts/JES/MapInfoSO.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+/// <summary>
+/// 맵의 정보를 담아두는 SO
+/// </summary>
+[CreateAssetMenu(menuName = "SO/Map/MapInfo")]
+public class MapInfoSO : ScriptableObject
+{
+    private Tilemap _floorTilemap,_colliderTilemap;//플로어 타일맵, 콜라이더 타일맵
+    
+    public void Initialize(Tilemap floorTilemap, Tilemap colliderTilemap)
+    {
+        _floorTilemap = floorTilemap;
+        _colliderTilemap = colliderTilemap;
+    }
+    
+    
+    
+    public Vector3Int GetTilePosFromWorld(Vector3 from) => _floorTilemap.WorldToCell(from); //월드 좌표로 바닥 타일맵의 좌표를 반환
+    public Vector3 GetCellCenterToWorld(Vector3Int cellCenter) => _floorTilemap.GetCellCenterWorld(cellCenter); //타일맵의 중심의 좌표를 월드좌표로 변환
+}
